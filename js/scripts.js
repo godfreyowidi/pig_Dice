fcc48bc0bc42fd893778deb31fa207f595ea34d8
Godfrey Owidi <godfreyowiidi@gmail.com>
2021-06-17 02:27:53 +0300

    pig dice game.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
 function Player(playerNumber, currentRoll, currentTurnScore, turnScore, totalScore, status) {
-  this.playerNumber = player;
+  this.playerNumber = playerNumber;
   this.currentRoll = currentRoll;
-  this.currentTurnScore = currentTurnScrore;
+  this.currentTurnScore = currentTurnScore;
   this.turnScore = turnScore;
   this.totalScore = totalScore;
   this.status = status;
 }
 
-let player1 = new Player(1, 0, 0, [], 0, false);
+// let player1 = new Player(1, 0, 0, [], 0, false);
 
-let player2 = new Player(2, 0, 0, [], 0, false);
+// let player2 = new Player(2, 0, 0, [], 0, false);
 
 let rollValuesArray = [];
 
@@ -33,3 +33,79 @@ Player.prototype.endTurn = function() {
   rollValuesArray = [];
   this.currentTurnScore = 0;
 };
+
+Player.prototype.calcTurnScore = function() {
+  let sum = 0;
+  rollValuesArray.forEach(function(element) {
+    sum += element;
+  });
+  this.currentTurnScore = sum;
+};
+
+Player.prototype.calcTotalScore = function() {
+let sum = 0;
+this.turnScore.forEach(function(element) {
+  sum += element;
+});
+this.totalScore = sum;
+};
+
+Player.prototype.gameOver = function() {
+  if (this.totalScore >= 20) {
+    $("#gameResults").show();
+    $(".gameBoard").hide();
+    $("#winner").text(this.playerNumber);
+  };
+};
+
+//UI LOGIC
+
+$(document).ready(function() {
+let player1 = new Player(1, 0, 0, [], 0, false);
+let player2 = new Player(2, 0, 0, [], 0, false);
+  $("#p1-roll").click(function() {
+    player1.rollDice();
+    $("#dice-roll").text(player1.currentRoll);
+    if (player1.currentRoll === 1) {
+      player1.currentTurnScore = 0;
+      $("#p1-roll").hide();
+    }
+    player1.calcTurnScore();
+    $(".p1-currentTurnScore").text(player1.currentTurnScore);
+  });
+
+  $("#p2-roll").click(function() {
+    player2.rollDice();
+    $("#dice-roll").text(player2.currentRoll);
+    if (player2.currentRoll === 1) {
+      player2.currentTurnScore = 0;
+      $("#p2-roll").hide();
+    }
+    player2.calcTurnScore();
+    $(".p2-currentTurnScore").text(player2.currentTurnScore);
+  });
+
+  $("#p1-pass").click(function() {
+    player1.endTurn();
+    player1.calcTotalScore();
+    $(".player-2").show();
+    $(".player-1").slideUp();
+    $("#p1-total").text(player1.totalScore);
+    $("#p2-roll").show();
+    $(".p1-score").text(player1.currentTurnScore);
+    player1.gameOver();
+  });
+
+  $("#p2-pass").click(function() {
+    player2.endTurn();
+    player2.calcTotalScore();
+    $(".player-1").show();
+    $(".player-2").slideUp();
+    $("#p2-total").text(player2.totalScore);
+    $("#p1-roll").show();
+    $(".p2-score").text(player2.currentTurnScore);
+    player2.gameOver();
+  });
+
+
+});
